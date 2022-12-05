ba598fe844f7cb76ad6a868f88022bc45b9fcfdf
YAMISHKA02 <yashkur.misha@gmail.com>
2022-12-05 19:56:55 +0300

    better

--- Hardhat TS project/hardhat.config.ts
+++ Hardhat TS project/hardhat.config.ts
@@ -2,10 +2,9 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import'@openzeppelin/hardhat-upgrades';
 import "@nomicfoundation/hardhat-chai-matchers";
-import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
 import 'hardhat-deploy';
-import "hardhat-deploy-ethers";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
